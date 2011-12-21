f96271f139aa958d4f6de194317465f44e9fa603
Patrick Klingemann <patrick.klingemann@gmail.com>
2011-12-21 01:18:11 -0500

    refactoring

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,26 @@
 class User < ActiveRecord::Base
+  FILTERS = [:state]
   def self.filters(filter_params)
     filter_params = {} if filter_params.nil?
-    [
-      { :filter_slug => 'state', :name => 'State', :options => state_filters(filter_params[:state]) }
-    ]
+    FILTERS.map do |filter|
+      send("#{filter}_filter", send("#{filter}_filters", filter_params[filter]))
+    end
+  end
+
+  def self.state_filter_name
+    'State'
+  end
+
+  def self.state_filter_slug
+    'state'
+  end
+
+  def self.state_filter(options)
+    {
+      :filter_slug => state_filter_slug,
+      :name => state_filter_name,
+      :options => options
+    }
   end
 
   def self.state_filter_options
@@ -14,16 +31,21 @@ class User < ActiveRecord::Base
     group(:state).count
   end
 
+  def self.state_filter_option(state, selected)
+    {
+      :slug => state,
+      :name => state.capitalize,
+      :value => state,
+      :count => @counts[state],
+      :selected => selected
+    }
+  end
+
   def self.state_filters(filter_params)
-    counts = state_filter_counts
+    @counts = state_filter_counts
     state_filter_options.map do |state|
-      {
-        :slug => state,
-        :name => state.capitalize,
-        :value => state,
-        :count => counts[state],
-        :selected => filter_params.nil? ? true : filter_params.include?(state)
-      }
+      selected = filter_params.nil? ? true : filter_params.include?(state)
+      state_filter_option(state, selected)
     end
   end
 
